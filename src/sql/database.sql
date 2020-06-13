45f4a5a526f624465fe9b1461161911079d88167
Alex Rosso <alexrosso1025@gmail.com>
2020-06-13 16:01:57 -0500

    first commit

---
+++ src/sql/database.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE news_portal;
+USE news_portal;
+
+CREATE TABLE news(
+	id_news INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(100),
+    news TEXT,
+    data_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+DESCRIBE news;
+
+INSERT INTO news(title,news) values ('Matarife','La historia de un genocida innombrable');
+
+SELECT * FROM news;
